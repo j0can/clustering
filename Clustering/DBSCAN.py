9378a7174f1c688b530cedd5d8626357ff325479
Josef Can <90180324+j0can@users.noreply.github.com>
2023-08-15 10:04:53 +0200

    update todos to resolved

--- Clustering/DBSCAN.py
+++ Clustering/DBSCAN.py
@@ -6,43 +6,39 @@ import copy
 import random
 import math
 
-#TODO: Write your full name.
+
 name = 'Your Name'
-#ENDTODO
 
-# TODO: Selest one of the two problems by commenting the rest of the lines using # before each line
-#_, problem, database = CL.uploadDataBase1() # Function that upload the database 1
-_, problem, database = CL.uploadDataBase2() # Function that upload the database 2
-# ENDTODO
-#print (len(examples))
 
-# Important parameters used in DBSCAN
-# TODO: Change these parameters so Genetic algorithms works better.
-R = 10 # Radius to check neighbouring points. Positive number.
-minPoints = 2 # Minimum number of points within the radius to be consider a cluster. Positive integer.
-# ENDTODO
+_, problem, database = CL.uploadDataBase2()
 
-CL.DBSCAN_checkErrorsInParameters(R, minPoints) # Function that will double-check if your parameters are in the correct range.
 
-nonVisited, visited, actualCluster, visitedColor, action, nextToVisit = CL.DBSCAN_initialize(database) # Function that will initialize the algorithm
 
-CL.DBSCAN_visualize(nonVisited, visited, visitedColor, [], [], R, name) # Function to visualize the data and how the algorithm works
+R = 10
+minPoints = 2
 
-while True: # Main loop of the program that will run forever
 
-    # If there are not next to visit datapoints, one is randomly selected.
+CL.DBSCAN_checkErrorsInParameters(R, minPoints)
+
+nonVisited, visited, actualCluster, visitedColor, action, nextToVisit = CL.DBSCAN_initialize(database)
+
+CL.DBSCAN_visualize(nonVisited, visited, visitedColor, [], [], R, name)
+
+while True:
+
+    
     if nextToVisit == []:
         index = random.randint(0, len(nonVisited)-1)
         nextToVisit.append(index)
         action = False
 
-    neighbours = CL.DBSCAN_checkNeighbours(nonVisited, nextToVisit, R) # Function that will find the neighbours of the recently visited datapoints
+    neighbours = CL.DBSCAN_checkNeighbours(nonVisited, nextToVisit, R)
 
     nextToVisit, visited, nonVisited, visitedColor, aura, auraColor, actualCluster, action = CL.DBSCAN_updateVisited(nextToVisit, visited, nonVisited, neighbours, action, actualCluster, visitedColor, minPoints) # Function that will update the visited nodes
 
-    CL.DBSCAN_visualize(nonVisited, visited, visitedColor, aura, auraColor, R, name) # Function to visualize the data and how the algorithm works
+    CL.DBSCAN_visualize(nonVisited, visited, visitedColor, aura, auraColor, R, name)
 
-    if len(nonVisited) == 0: # If all the datapoints have been visited
-        break # If the condition in the line above, break will force the while loop to stop
+    if len(nonVisited) == 0:
+        break
 
-CL.DBSCAN_saveResults(nonVisited, visited, visitedColor, name, problem) # Function that save the results of the algorithm
+CL.DBSCAN_saveResults(nonVisited, visited, visitedColor, name, problem)
